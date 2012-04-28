ba7cf445239c964c6723ff71427b433c7e385b38
Thiago Hersan <thiago.hersan+github@gmail.com>
2012-04-28 02:25:54 -0400

    cleanup

--- src/com/hersan/foil/FoilActivityInput.java
+++ src/com/hersan/foil/FoilActivityInput.java
@@ -1,20 +1,12 @@
 package com.hersan.foil;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Intent;
-import android.content.DialogInterface;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.facebook.android.AsyncFacebookRunner;
 
